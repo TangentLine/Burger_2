9577b383a07f835c1318d1a63d875b654c539f1b
Carlvis Jones <c.jones299@gmail.com>
2017-08-17 19:28:51 -0400

    initial

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 ### Schema
 
-CREATE DATABASE cat_db;
+CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burger
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
